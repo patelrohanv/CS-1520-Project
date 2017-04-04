3f221973cd450533a75c2de164cd2f0aa4ad764d
patelrohanv <rohanpatel@Rohans-MacBook-Pro.local>
2017-04-04 13:37:57 -0400

    Generating work

--- pages/work.php
+++ pages/work.php
@@ -23,18 +23,16 @@
                             <span>Giant Eagle</span>
                         </div>
                         <div class="collapsible-body" class="myInfo">
-                            <span>June 2016 - August 2016</span>
-                            <br>
-                            <span>Was part of the Digital Commerce department to add features for merging, splitting and transferring user accounts</span>
-                            <br>
-                            <br>
-                            <span>Tested code under development where I optimized SQL queries and found where they were breaking </span>
-                            <br>
-                            <br>
-                            <span>Did a cross­-departmental project on Generational Diversity and Inclusion initiatives in order to attract and retain younger talent and keep the workplace friendly for more seasoned employees</span>
-                            <br>
-                            <br>
-                            <span>Presented cross­-departmental project to company executives with timeline, budget, and projected savings</span>
+                            <?php
+                                $file_handle = fopen("../info/giantEagle.txt", "r");
+                                while (!feof($file_handle)) {
+                                    $line = fgets($file_handle);
+                                    echo "<span>" . $line . "</span>";
+                                    echo "<br>";
+                                    echo "<br>";
+                                }
+                                fclose($file_handle);
+                            ?> 
                         </div>
                     </li>
                     <li>
@@ -42,12 +40,16 @@
                             <span>CosmoLex</span>
                         </div>
                         <div class="collapsible-body" class="myInfo">
-                            <span>April 2016 - May 2016</span>
-                            <br>
-                            <span>Tested software efficiency by tracing function calls and data queries in order to determine which calls and queries could more efficient or removed</span>
-                            <br>
-                            <br>
-                            <span>Created a Windows Form application to read list of servers (Windows and Linux) to a table that users could automatically log into</span>
+                            <?php
+                                $file_handle = fopen("../info/cosmolex.txt", "r");
+                                while (!feof($file_handle)) {
+                                    $line = fgets($file_handle);
+                                    echo "<span>" . $line . "</span>";
+                                    echo "<br>";
+                                    echo "<br>";
+                                }
+                                fclose($file_handle);
+                            ?> 
                         </div>
                     </li>
                     <li>
@@ -55,21 +57,16 @@
                             <span>Swanson School of Engineering</span>
                         </div>
                         <div class="collapsible-body" class="myInfo">
-                            <span>August 2015 - August 2015</span>
-                            <br>
-                            <span>Managed the computer labs and assisted personnel such as professors, faculty and administrators</span>
-                            <br>
-                            <br>
-                            <span>Answered calls at the help desk for technical support</span>
-                            <br>
-                            <br>
-                            <span>Created and resolved support tickets</span>
-                            <br>
-                            <br>
-                            <span>Rebuilt and repaired computers, printers and other devices Managed and took care of the computer labs</span>
-                            <br>
-                            <br>
-                            <span>Handled other such technology related problems</span>
+                            <?php
+                                $file_handle = fopen("../info/benedum.txt", "r");
+                                while (!feof($file_handle)) {
+                                    $line = fgets($file_handle);
+                                    echo "<span>" . $line . "</span>";
+                                    echo "<br>";
+                                    echo "<br>";
+                                }
+                                fclose($file_handle);
+                            ?> 
                         </div>
                     </li>
                 </ul>
